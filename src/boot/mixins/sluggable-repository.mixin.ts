9cc7cf8b9e99b88b130d08b412cd1f3b568c7277
m-ich-dev <m.korotkevich.dev@gmail.com>
2026-03-13 07:33:08 +0300

    add sluggable mixins for services and repositories

---
+++ src/boot/mixins/sluggable-repository.mixin.ts
@@ -0,0 +1,39 @@
+import { Kysely } from "kysely";
+import { IDatabase } from "../database/schemas/index.schema";
+
+interface IRepository {
+  tableName: keyof IDatabase;
+}
+
+type AbstractConstructor<T = any> = abstract new (...args: any[]) => T;
+
+export function Sluggable<TBase extends AbstractConstructor<IRepository>>(Base: TBase) {
+  abstract class SluggableRepository extends Base {
+
+    protected abstract readonly db: Kysely<IDatabase>;
+    public abstract readonly tableName: keyof IDatabase;
+
+    async lastSlugIndex(baseSlug: string): Promise<number | null> {
+
+      const row = await this.db
+        .selectFrom(this.tableName)
+        .select('slug')
+        .where('slug', 'like', `${baseSlug}%`)
+        .orderBy('slug', 'desc')
+        .limit(1)
+        .executeTakeFirst();
+
+      if (!row) return null;
+
+      if (row.slug === baseSlug) return 0;
+
+      const suffix = row.slug.slice(baseSlug.length + 1);
+
+      const num = Number(suffix);
+
+      return Number.isNaN(num) ? 0 : num;
+    }
+
+  }
+  return SluggableRepository;
+}
---
+++ src/boot/mixins/sluggable-service.mixin.ts
@@ -0,0 +1,30 @@
+import slugify from "../utils/slugify";
+
+type AbstractConstructor<T = any> = abstract new (...args: any[]) => T;
+
+interface SlugRepository {
+    lastSlugIndex(baseSlug: string): Promise<number | null>;
+}
+export function GenerateSlug<TBase extends AbstractConstructor>(Base: TBase) {
+
+    abstract class SluggableService extends Base {
+
+        protected async generateSlug(
+            repository: SlugRepository,
+            fields: (string | number | null | undefined)[] | string
+        ): Promise<string> {
+
+            const baseSlug = slugify(fields);
+
+            const lastIndex = await repository.lastSlugIndex(baseSlug);
+
+            if (lastIndex === null) {
+                return baseSlug;
+            }
+
+            return `${baseSlug}-${lastIndex + 1}`;
+        }
+    }
+
+    return SluggableService;
+}
