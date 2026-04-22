96f1bfcbccb178ac2ee80335a5e6ebbca8122eab
Maksim Korotkevich <m.korotkevich.dev@gmail.com>
2026-04-22 19:09:08 +0300

    complete base crud admin controller for variant

--- src/http/v1/controllers/admin/product-variant.admin.controller.ts
+++ src/http/v1/controllers/admin/product-variant.admin.controller.ts
@@ -5,15 +5,35 @@ import ProductVariantService from "../../../../services/product-variant.service"
 
 
 export default class ProductVariantAdminController extends Controller {
-    constructor(private readonly variantService: ProductVariantService) { super(); }
+    constructor(
+        private readonly variantService: ProductVariantService
+    ) { super(); }
 
     public index: THttp = async (req, res) => {
-        const variants = this.variantService.all();
+        const variants = await this.variantService.all();
         return res.status(HTTP_CODES.OK).json({ data: variants });
     };
+
+    public store: THttp = async (req, res) => {
+        const variant = await this.variantService.store(req.body);
+        return res.status(HTTP_CODES.OK).json({ data: variant });
+    };
+
     public show: THttpLocals<{ slug: string }> = async (req, res) => {
         const slug = res.locals.slug;
-        const variant = this.variantService.showBySlug(slug);
+        const variant = await this.variantService.showBySlug(slug);
         return res.status(HTTP_CODES.OK).json({ data: variant });
+    };
+
+    public update: THttpLocals<{ slug: string }> = async (req, res) => {
+        const slug = res.locals.slug;
+        const variant = await this.variantService.update(req.body, slug);
+        return res.status(HTTP_CODES.OK).json({ data: variant });
+    };
+
+    public destroy: THttpLocals<{ slug: string }> = async (req, res) => {
+        const slug = res.locals.slug;
+        const result = await this.variantService.delete(slug);
+        return res.status(HTTP_CODES.OK).json({ data: result });
     };
 }
