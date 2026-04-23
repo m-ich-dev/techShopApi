f7cc7c9c6b16e47b234837212671221078b9ed24
m-ich-dev <m.korotkevich.dev@gmail.com>
2026-04-23 13:51:05 +0300

    add sluggable and softdeletable table types

--- src/boot/database/schemas/index.schema.ts
+++ src/boot/database/schemas/index.schema.ts
@@ -1,21 +1,21 @@
-import * as attributeSchema from "./attribute.schema";
-import * as brandSchema from "./brand.schema";
-import * as categorySchema from "./category.schema";
-import * as priceSchema from "./price.schema";
-import * as productSchema from "./product.schema";
-import * as productVariantSchema from "./product-variant.schema";
-import * as productVariantAttributeSchema from "./product-variant-atrribute.schema";
 import { Insertable, Selectable, Updateable } from "kysely";
+import { ICategoryTable } from "./category.schema";
+import { IBrandTable } from "./brand.schema";
+import { IAttributeTable } from "./attribute.schema";
+import { IPriceTable } from "./price.schema";
+import { IProductTable } from "./product.schema";
+import { IProductVariantTable } from "./product-variant.schema";
+import { IProductVariantAttributeTable } from "./product-variant-atrribute.schema";
 
 
 export interface IDatabase {
-    categories: categorySchema.ICategoryTable;
-    brands: brandSchema.IBrandTable;
-    attributes: attributeSchema.IAttributeTable;
-    prices: priceSchema.IPriceTable;
-    products: productSchema.IProductTable;
-    productVariants: productVariantSchema.IProductVariantTable;
-    productVariantAttributes: productVariantAttributeSchema.IProductVariantAttributeTable;
+    categories: ICategoryTable;
+    brands: IBrandTable;
+    attributes: IAttributeTable;
+    prices: IPriceTable;
+    products: IProductTable;
+    productVariants: IProductVariantTable;
+    productVariantAttributes: IProductVariantAttributeTable;
 }
 
 export type TSelectable = {
@@ -26,4 +26,10 @@ export type TInsertable = {
 };
 export type TUpdateable = {
     [K in keyof IDatabase]: Updateable<IDatabase[K]>
+};
+export type TSluggable = {
+    [K in keyof IDatabase as 'slug' extends keyof IDatabase[K] ? K : never]: IDatabase[K]
+};
+export type TSoftDeletable = {
+    [K in keyof IDatabase as 'deletedAt' extends keyof IDatabase[K] ? K : never]: IDatabase[K]
 };
