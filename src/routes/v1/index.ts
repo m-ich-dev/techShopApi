76041a290c2c78ed9c5ece78d9c85412830594ef
Maksim Korotkevich <m.korotkevich.dev@gmail.com>
2026-05-12 19:30:37 +0300

    add auth mwares to admin routes

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import adminRouter from '@/routes/v1/admin/index.js';
 import storeRouter from '@/routes/v1/store/index.js';
+import authMiddleware from "@/middlewares/auth/auth.middleware.js";
+import roleMiddleware from "@/middlewares/auth/role.middleware.js";
 
 
 const v1Router = Router();
 
-v1Router.use('/admin', adminRouter);
+v1Router.use('/admin', authMiddleware, roleMiddleware([1, 0]), adminRouter);
 v1Router.use(storeRouter);
 
 export default v1Router;
