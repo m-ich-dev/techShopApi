d57e1a562e13820b23be2393020396f9101af002
Maksim Korotkevich <m.korotkevich.dev@gmail.com>
2026-05-24 21:41:36 +0300

    add pagination links generation in base controller

--- src/boot/http/controller.ts
+++ src/boot/http/controller.ts
@@ -1,1 +1,25 @@
-export default abstract class Controller { }
+import type { Request } from "express";
+import type { TPaginateMeta } from "../types/repository.types.js";
+
+
+export default abstract class Controller {
+
+    protected currentUrl(req: Request) {
+        const url = req.get('host') + req.originalUrl;
+        return url;
+    }
+    private createPaginationLink(req: Request, targetPage: number) {
+        const url = new URL(this.currentUrl(req));
+        url.searchParams.set('page', targetPage.toString());
+        return url.toString();
+    }
+    protected paginationLinks(req: Request, meta: TPaginateMeta) {
+        return {
+            self: this.currentUrl(req),
+            prev: meta.page > 1 ? this.createPaginationLink(req, meta.prev) : null,
+            next: meta.page < meta.last ? this.createPaginationLink(req, meta.next) : null,
+            first: this.createPaginationLink(req, meta.first),
+            last: this.createPaginationLink(req, meta.last)
+        };
+    }
+}
