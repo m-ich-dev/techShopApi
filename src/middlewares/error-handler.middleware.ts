cd54cd879488ed6636a9bb79e8cec35526bde3b2
Maksim Korotkevich <m.korotkevich.dev@gmail.com>
2026-05-24 23:54:02 +0300

    use response formatter in error handler

--- src/middlewares/error-handler.middleware.ts
+++ src/middlewares/error-handler.middleware.ts
@@ -1,39 +1,32 @@
 import type { NextFunction, Request, Response } from "express";
 import HTTPError from "@/boot/http/http.error.js";
 import { DatabaseError } from "pg";
-import { HTTP_CODES, HTTP_TITLES } from "@/boot/enums/http.enum.js";
 import { JOSEError } from "jose/errors";
+import ResponseFormatter from "@/boot/http/response.formatter.js";
 
 
 export function errorHandler(error: unknown, req: Request, res: Response, next: NextFunction) {
     if (error instanceof HTTPError) {
-        return res.status(error.status).json({
-            name: error.name,
-            title: error.title,
-            message: error.message,
-            detail: error.detail
+        const { status, errors } = ResponseFormatter.HTTPError(error);
+        return res.status(status).json({
+            errors
         });
     } else if (error instanceof DatabaseError) {
-        if (error.code === '23503') {
-            return res.status(HTTP_CODES.NOT_FOUND).json({
-                name: 'HTTP API ERROR',
-                title: HTTP_TITLES['404'],
-                message: `${error.constraint} not found`,
-                detail: { path: 'foreign key', message: error.detail }
-            });
-        }
+        const { status, errors } = ResponseFormatter.DBError(error);
+
+        return res.status(status).json({
+            errors
+        });
     } else if (error instanceof JOSEError) {
-        return res.status(HTTP_CODES.UNAUTHORIZED).json({
-            name: 'HTPP API ERROR',
-            title: HTTP_TITLES['404'],
-            message: 'authentication failure',
-            detail: { path: 'auth', message: error.code }
+        const { status, errors } = ResponseFormatter.JWT(error);
+        return res.status(status).json({
+            errors
         });
     }
     else if (error instanceof Error) {
-        return res.status(500).json({
-            message: 'Unknown server error',
-            stack: process.env.NODE_ENV === 'development' ? error.stack : undefined
+        const { status, errors } = ResponseFormatter.Error(error);
+        return res.status(status).json({
+            errors
         });
     }
 }
