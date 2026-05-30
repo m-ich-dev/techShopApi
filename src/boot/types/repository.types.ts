55983bb1824cd07681f205a4765a09ec91c9e43c
Maksim Korotkevich <m.korotkevich.dev@gmail.com>
2026-05-30 19:39:49 +0300

    feat(filter): add filter applier and integrate with paginate

--- src/boot/types/repository.types.ts
+++ src/boot/types/repository.types.ts
@@ -1,9 +1,14 @@
+import type { SelectQueryBuilder } from "kysely";
+
+
+export type TQuery = SelectQueryBuilder<any, any, any>;
+export type TBuild = (qb: TQuery) => TQuery;
 export type TWhereParams<C, V> = { column: C, value: V, withTrash?: boolean };
 export type TSelectParams = Partial<{ withTrash?: boolean }>;
 export type TUpdateParams<C, V> = { column: C, value: V };
 export type TDeleteParams<C, V> = { column: C, value: V };
 export type TSoftDeleteParams<C, V> = { column: C, value: V };
-export type TPaginateParams = Partial<{ page: number, limit: number, withTrash: boolean }>;
+export type TPaginateParams = Partial<{ page: number, limit: number, withTrash: boolean, build?: TBuild | TBuild[] }>;
 export type TPaginateMeta = {
     page: number,
     next: number,
