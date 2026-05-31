4698ff0648df4ee10bfa34e03d737e9cb77c71ff
Maksim Korotkevich <m.korotkevich.dev@gmail.com>
2026-05-31 19:15:09 +0300

    add filter builder for product pivot pagination, improve paginate types

--- src/boot/types/repository.types.ts
+++ src/boot/types/repository.types.ts
@@ -2,13 +2,14 @@ import type { SelectQueryBuilder } from "kysely";
 
 
 export type TQuery = SelectQueryBuilder<any, any, any>;
-export type TBuild = (qb: TQuery) => TQuery;
+export type TBuild<Q extends TQuery> = (qb: Q) => Q;
+
 export type TWhereParams<C, V> = { column: C, value: V, withTrash?: boolean };
 export type TSelectParams = Partial<{ withTrash?: boolean }>;
 export type TUpdateParams<C, V> = { column: C, value: V };
 export type TDeleteParams<C, V> = { column: C, value: V };
 export type TSoftDeleteParams<C, V> = { column: C, value: V };
-export type TPaginateParams = Partial<{ page: number, limit: number, withTrash: boolean, build?: TBuild | TBuild[] }>;
+export type TPaginateParams<Q extends TQuery = TQuery> = Partial<{ page: number, limit: number, withTrash: boolean, build?: TBuild<Q> | TBuild<Q>[] }>;
 export type TPaginateMeta = {
     page: number,
     next: number,
