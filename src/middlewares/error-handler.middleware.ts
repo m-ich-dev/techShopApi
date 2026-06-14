d1e1a9240d6f19b20eb873608c06c7a869e452d8
Maksim Korotkevich <m.korotkevich.dev@gmail.com>
2026-06-14 16:11:21 +0300

    feat(middleware): integrate logger into error handler

--- src/middlewares/error-handler.middleware.ts
+++ src/middlewares/error-handler.middleware.ts
@@ -6,25 +6,45 @@ import ResponseFormatter from "@/boot/http/response.formatter.js";
 
 
 export function errorHandler(error: unknown, req: Request, res: Response, next: NextFunction) {
+
     if (error instanceof HTTPError) {
         const { status, errors } = ResponseFormatter.HTTPError(error);
+        if (error.status === 500) {
+            req.log.error({
+                errors
+            });
+        }
+        req.log.warn({
+            status,
+            err: error,
+
+            apiErrors: errors,
+        }, 'request failed');
         return res.status(status).json({
             errors
         });
     } else if (error instanceof DatabaseError) {
         const { status, errors } = ResponseFormatter.DBError(error);
-
+        req.log.error({
+            errors
+        });
         return res.status(status).json({
             errors
         });
     } else if (error instanceof JOSEError) {
         const { status, errors } = ResponseFormatter.JWT(error);
+        req.log.warn({
+            errors
+        });
         return res.status(status).json({
             errors
         });
     }
     else if (error instanceof Error) {
         const { status, errors } = ResponseFormatter.Error(error);
+        req.log.error({
+            errors
+        });
         return res.status(status).json({
             errors
         });
