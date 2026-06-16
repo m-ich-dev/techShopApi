aa5d2b0b51edf17897672e0fededb789aaa2e248
m-ich-dev <m.korotkevich.dev@gmail.com>
2026-06-16 13:42:26 +0300

    feat(logger): add pino-pretty for dev env

--- src/boot/loggers/config/logger.config.ts
+++ src/boot/loggers/config/logger.config.ts
@@ -5,4 +5,10 @@ const pinoConfig: LoggerOptions = {
     level: process.env.MIN_LOG_LEVEL ?? "info",
 };
 
+if (process.env.NODE_ENV === 'development') {
+    pinoConfig.transport = {
+        target: 'pino-pretty',
+    };
+}
+
 export default pinoConfig;
