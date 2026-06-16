4262736a3cfc2ebde1400e873a9b4d8c24b1e931
m-ich-dev <m.korotkevich.dev@gmail.com>
2026-06-16 13:30:44 +0300

    chore(logger): move logger to loggres folder

--- src/boot/loggers/logger.ts
+++ src/boot/loggers/logger.ts
@@ -1,11 +1,18 @@
-import pino from "pino";
+import pino, { type LoggerOptions } from "pino";
 import { pinoHttp } from "pino-http";
 
-const logger = pino({
-    level:
-        process.env.MIN_LOG_LEVEL
-        ?? "info"
-});
+
+const config: LoggerOptions = {
+    level: process.env.MIN_LOG_LEVEL ?? "info",
+};
+
+if (process.env.NODE_ENV === 'development') {
+    config.transport = {
+        target: 'pino-pretty',
+    };
+}
+
+const logger = pino(config);
 
 export const httpLogger =
     pinoHttp({
