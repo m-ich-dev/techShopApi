4d2456001775c58bc4a0e7c750a25ce3f095f903
m-ich-dev <m.korotkevich.dev@gmail.com>
2026-06-16 15:00:26 +0300

    feat(logger): timestamped

--- src/boot/loggers/config/logger.config.ts
+++ src/boot/loggers/config/logger.config.ts
@@ -1,8 +1,10 @@
 import type { LoggerOptions } from "pino";
+import pino from "pino";
 
 
 const pinoConfig: LoggerOptions = {
     level: process.env.MIN_LOG_LEVEL ?? "info",
+    timestamp: pino.stdTimeFunctions.isoTime
 };
 
 if (process.env.NODE_ENV === 'development') {
