715f5d6cfe808f7dc1c1e651993871fb5dcccfef
Maksim Korotkevich <m.korotkevich.dev@gmail.com>
2026-06-17 20:13:06 +0300

    feat(http-logger): add request id

--- src/boot/loggers/config/http-logger.config.ts
+++ src/boot/loggers/config/http-logger.config.ts
@@ -1,15 +1,24 @@
 import logger from "@/boot/loggers/logger.js";
 import type { Options } from "pino-http";
 import type { IncomingMessage, ServerResponse } from "node:http";
+import crypto from 'node:crypto';
 
 
 const httpPinoConfig: Options = {
     logger,
     autoLogging: false,
+    genReqId: (req, res) => {
+        const existingId = req.headers['x-correlation-id'];
+        if (existingId) return existingId;
 
+        const id = crypto.randomBytes(8).toString('hex');
+        res.setHeader('x-correlation-id', id);
+        return id;
+    },
     serializers: {
         req(req: IncomingMessage) {
             return {
+                reqId: req.id,
                 method: req.method,
                 url: req.url,
                 ip: req.headers['x-forwarded-for'] || req.socket?.remoteAddress || 'unknown',
