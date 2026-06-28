708e9b6ebce942e6e64ab345ebc2d4915f0ebfc0
Maksim Korotkevich <m.korotkevich.dev@gmail.com>
2026-06-28 14:20:47 +0300

    feat(db): implement automated migrations and refactor database module (#5)
    * refactor(db): separate database instance and config
    
    * feat(db): add database migration runner
    
    * feat(db): add migrate-latest script for prod
    
    * feat(db): add single-step rollback script
    
    * chore: add profiles for container
    
    * chore: add migration step in workflow

---
+++ src/boot/database/kysely.conf.ts
@@ -0,0 +1,30 @@
+import type { MigrationProvider } from "kysely";
+import { FileMigrationProvider, PostgresDialect } from "kysely";
+import { Pool, type PoolConfig } from "pg";
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
+
+const PORT = Number(process.env.APP_DB_PORT) || 5432;
+const MIN = Number(process.env.APP_DB_MIN_CONS) || 2;
+const MAX = Number(process.env.APP_DB_MAX_CONS) || 10;
+
+const pgConnection: PoolConfig = {
+    database: process.env.APP_DB_NAME,
+    host: process.env.APP_DB_HOST,
+    user: process.env.APP_DB_USER,
+    password: process.env.APP_DB_PASSWORD,
+    port: PORT,
+    min: MIN,
+    max: MAX,
+};
+
+export const fsProvider: MigrationProvider = new FileMigrationProvider({
+    fs,
+    path,
+    migrationFolder: path.resolve(process.cwd(),'dist/database/migrations')
+});
+
+export const dialect = new PostgresDialect({
+    pool: new Pool(pgConnection)
+});
