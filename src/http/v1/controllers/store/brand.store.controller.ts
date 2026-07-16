34c9cd09370c4fab9f91b35ef90e3b8e02f636f2
Maksim Korotkevich <m.korotkevich.dev@gmail.com>
2026-07-16 13:29:13 +0200

    chore: off brand store, category store

--- src/http/v1/controllers/store/brand.store.controller.ts
+++ src/http/v1/controllers/store/brand.store.controller.ts
@@ -1,20 +1,20 @@
 import Controller from "@/boot/http/controller.js";
-import BrandResource from "@/http/v1/resources/brand/brand.resource.js";
+// import BrandResource from "@/http/v1/resources/brand/brand.resource.js";
+// import type { THttp, THttpLocals } from "@/boot/types/http.types.js";
 import type BrandService from "@/services/brand.service.js";
-import type { THttp, THttpLocals } from "@/boot/types/http.types.js";
 
 
 export default class BrandStoreController extends Controller {
     constructor(private readonly brandService: BrandService) { super(); }
 
-    public index: THttp = async (req, res) => {
-        const brands = await this.brandService.all();
-        return this.resOk(res, { data: BrandResource.collection(brands) });
-    };
+    // public index: THttp = async (req, res) => {
+    //     const brands = await this.brandService.all();
+    //     return this.resOk(res, { data: BrandResource.collection(brands) });
+    // };
     
-    public show: THttpLocals<{ slug: string }> = async (req, res) => {
-        const slug = res.locals.slug;
-        const brand = await this.brandService.showBySlug(slug);
-        return this.resOk(res, { data: BrandResource.transform(brand) });
-    };
+    // public show: THttpLocals<{ slug: string }> = async (req, res) => {
+    //     const slug = res.locals.slug;
+    //     const brand = await this.brandService.showBySlug(slug);
+    //     return this.resOk(res, { data: BrandResource.transform(brand) });
+    // };
 }
--- src/routes/v1/store/index.ts
+++ src/routes/v1/store/index.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import brandRouter from "@/routes/v1/store/brand.routes.js";
-import categoryRouter from "@/routes/v1/store/category.routes.js";
+// import brandRouter from "@/routes/v1/store/brand.routes.js";
+// import categoryRouter from "@/routes/v1/store/category.routes.js";
 import productRouter from "@/routes/v1/store/product.routes.js";
 
 const router = Router();
 
-router.use('/brands', brandRouter);
-router.use('/categories', categoryRouter);
+// router.use('/brands', brandRouter);
+// router.use('/categories', categoryRouter);
 router.use('/products', productRouter);
 export default router;
